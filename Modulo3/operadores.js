e65d8ccd53637c7f03048a246e6ca19a8f97123f
Victoria Cardoso <vkycardoso@gmail.com>
2023-07-31 23:21:50 -0300

    Dia1

---
+++ Modulo3/operadores.js
@@ -0,0 +1,13 @@
+console.log(3 + 5);
+console.log(14 - 7);
+console.log(4 * 6);
+console.log(30 / 9);
+console.log(95 % 4);
+
+console.log(3 + 5 * 2 - 8);
+console.log((3 + 5) * 2 - 8);
+
+console.log("HOLA" + "CAMILO");
+var nombre = "CAMILO";
+var saludo = "HOLA" + nombre;
+console.log(saludo)
